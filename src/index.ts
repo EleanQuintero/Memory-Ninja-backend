f406562b1ee5a38914e12aed61d6a2bcc427ca1d
EleanQuintero <elean.quintero.madrid@gmail.com>
2025-07-18 01:08:00 +0200

    Security improvments

--- src/index.ts
+++ src/index.ts
@@ -1,33 +1,24 @@
 import express, { Request, Response } from 'express'
-import questionRouter from './routes/app'
 import appRouter from './routes/app'
 import userRouter from './routes/userRouter'
 import healthRouter from './routes/health'
+import helmet from 'helmet'
+import { limiter } from './services/rateLimiter'
 
 const PORT: number | string = process.env.PORT ?? 4444
 const app = express()
 
+
 app.use(express.json())
+app.use(helmet())
 app.disable('x-powered-by')
-app.use('/api/questions', appRouter)
-app.use('/api/user', userRouter)
-app.use('/api/health', healthRouter)
+app.use('/api/questions', limiter({minuteDuration: 1, maxRequest: 5}), appRouter)
+app.use('/api/user', limiter({minuteDuration: 1, maxRequest: 10}), userRouter)
+app.use('/api/health', limiter({minuteDuration: 1, maxRequest: 3}),  healthRouter)
 
 app.get('/', (req: Request, res: Response): void => {
     res.json({mensaje: "Bienvenido al punto de entrada de la API"})
 })
-
-/*app.post('/user', async (req: Request, res: Response): Promise<void> => {
-    try {
-        
-        const {id, name, lastName, userName, email, role} = req.body
-        console.log("📥 Usuario recibido:", { id, name, lastName, userName, email, role });
-
-        res.status(201).json({ message: "Usuario sincronizado correctamente" });
-    } catch (error) {
-        res.status(500).json({ error: 'Error al obtener datos del usuario' })
-    }
-})*/
 
 app.listen(PORT, () => {
     console.log(`Server running on PORT: ${PORT}`)
