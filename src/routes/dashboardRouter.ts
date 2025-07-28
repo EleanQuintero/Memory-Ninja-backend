bdd513c624f41a62ba193bda9db47ee4bede0f58
EleanQuintero <elean.quintero.madrid@gmail.com>
2025-07-28 20:27:55 +0200

    max flashcards by user endpoint

--- src/routes/dashboardRouter.ts
+++ src/routes/dashboardRouter.ts
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import { getCountFlashcardsByTheme } from "../controllers/dashboardData/getCountFlashcardsByTheme";
 import { getLastestFlashcardsCreated } from "../controllers/dashboardData/getLastestFlashcardsCreated";
+import { getMaxFlashcardsByUser } from "../controllers/dashboardData/getMaxFlashcardsByUser";
 
 const dashboardRouter = Router()
 
 dashboardRouter.get("/countByTheme/:userId", getCountFlashcardsByTheme)
 dashboardRouter.get("/latestFlashcards/:userId", getLastestFlashcardsCreated)
+dashboardRouter.get("/maxFlashcards/:userId", getMaxFlashcardsByUser)
 
 export default dashboardRouter
