be509ea3e163e1d98c2d8c98496ed9e98b1feeae
EleanQuintero <elean.quintero.madrid@gmail.com>
2025-07-28 20:24:15 +0200

    lastest flashcard route endpoint added

--- src/routes/dashboardRouter.ts
+++ src/routes/dashboardRouter.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import { getCountFlashcardsByTheme } from "../controllers/dashboardData/getCountFlashcardsByTheme";
+import { getLastestFlashcardsCreated } from "../controllers/dashboardData/getLastestFlashcardsCreated";
 
 const dashboardRouter = Router()
 
 dashboardRouter.get("/countByTheme/:userId", getCountFlashcardsByTheme)
+dashboardRouter.get("/latestFlashcards/:userId", getLastestFlashcardsCreated)
 
 export default dashboardRouter
