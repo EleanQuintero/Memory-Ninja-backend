30b6f6c86fa20d8bced3fa4e2a8e7120ba04fbed
EleanQuintero <elean.quintero.madrid@gmail.com>
2025-07-28 20:30:22 +0200

    get theme w/most flashcards endpoint

--- src/routes/dashboardRouter.ts
+++ src/routes/dashboardRouter.ts
@@ -2,11 +2,13 @@ import { Router } from "express";
 import { getCountFlashcardsByTheme } from "../controllers/dashboardData/getCountFlashcardsByTheme";
 import { getLastestFlashcardsCreated } from "../controllers/dashboardData/getLastestFlashcardsCreated";
 import { getMaxFlashcardsByUser } from "../controllers/dashboardData/getMaxFlashcardsByUser";
+import { getThemeWithMaxFlashcards } from "../controllers/dashboardData/getThemeWithMaxFlashcards";
 
 const dashboardRouter = Router()
 
 dashboardRouter.get("/countByTheme/:userId", getCountFlashcardsByTheme)
 dashboardRouter.get("/latestFlashcards/:userId", getLastestFlashcardsCreated)
 dashboardRouter.get("/maxFlashcards/:userId", getMaxFlashcardsByUser)
+dashboardRouter.get("/themeWithMaxFlashcards/:userId", getThemeWithMaxFlashcards)
 
 export default dashboardRouter
