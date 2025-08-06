ca5bd158a87e68db7444f381d327cb2c3e86c572
EleanQuintero <elean.quintero.madrid@gmail.com>
2025-08-06 02:03:17 +0200

    refactor: clean up request handling and remove userLevel from schemas

--- src/middlewares/validateQuestionsRequest.ts
+++ src/middlewares/validateQuestionsRequest.ts
@@ -1,18 +1,18 @@
 import { NextFunction, Request, Response } from "express";
 import { z } from "zod";
 
-export const validateBody = (schema: z.ZodType) => (req: Request, res: Response, next: NextFunction):void  => {
-    const result = schema.safeParse(req.body)
+export const validateBody = (schema: z.ZodType) => (req: Request, res: Response, next: NextFunction): void => {
+  const result = schema.safeParse(req.body)
 
-    if (!result.success) {
-        const errorDetails = result.error.format();
-        res.status(400).json({
-          error: 'Datos inválidos',
-          details: errorDetails.id._errors,
-        });
-        return;
-      }
-    
-      req.body = result.data; // ✅ datos parseados y tipados
-      next();
+  if (!result.success) {
+    const errorDetails = result.error.format();
+    res.status(400).json({
+      error: 'Datos inválidos',
+      details: errorDetails.id._errors,
+    });
+    return;
+  }
+
+  req.body = result.data; // ✅ datos parseados y tipados
+  next();
 }
