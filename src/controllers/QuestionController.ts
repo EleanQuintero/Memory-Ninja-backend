09437c5885c8e00f51d17100e00893952d583f07
EleanQuintero <elean.quintero.madrid@gmail.com>
2025-10-24 12:52:31 +0200

    changes error info

--- src/controllers/QuestionController.ts
+++ src/controllers/QuestionController.ts
@@ -5,25 +5,32 @@ import { resolveServices } from '../infrastructure/di/container'
 
 export async function askQuestion(req: QuestionRequest, res: Response): Promise<void> {
 
-    const { tema, questions, model } = req.body
-    const userLevel = req.user?.userLevel
-    console.log('Model selected:', model)
+    try {
+        const { tema, questions, model } = req.body
+        const userLevel = req.user?.userLevel
+        console.log('Model selected:', model)
 
-    if (!userLevel) {
-        res.status(400).json({ error: "User level is required" })
-        return
-    }
+        if (!userLevel) {
+            res.status(400).json({ error: "User level is required" })
+            return
+        }
 
-    const service = resolveServices({ userLevel, model })
+        const service = resolveServices({ model })
 
 
-    if (questions.length === 1) {
-        const answer = await service.handleQuestion(tema, questions)
+        if (questions.length === 1) {
+            const answer = await service.handleQuestion(tema, questions)
+            res.json({ answer })
+            return
+        }
+
+        const answer = await service.handleMultipleQuestion(tema, questions)
         res.json({ answer })
         return
+    } catch (error) {
+        console.error("Error completo en askQuestion:", error)
+        res.status(500).json({
+            error: "Error al procesar la pregunta",
+        })
     }
-
-    const answer = await service.handleMultipleQuestion(tema, questions)
-    res.json({ answer })
-    return
 }
--- src/services/QuestionService.ts
+++ src/services/QuestionService.ts
@@ -1,14 +1,15 @@
 import { IAInterface } from "../ia/IAInterface";
 
 export class QuestionService {
-    constructor(private iaModel: IAInterface) {}
+    constructor(private iaModel: IAInterface) { }
 
     async handleQuestion(tema: string, pregunta: string[]): Promise<string> {
         try {
             const answer = await this.iaModel.generateAnswer(tema, pregunta)
             return answer
         } catch (error) {
-            throw new Error("Error al procesar la pregunta")
+            console.error("Error detallado en handleQuestion:", error)
+            throw new Error("Error al generar la respuesta para una sola pregunta")
         }
     }
 
@@ -17,7 +18,8 @@ export class QuestionService {
             const answer = await this.iaModel.generateMultipleAnswer(tema, pregunta)
             return answer
         } catch (error) {
-            throw new Error("Error al procesar la pregunta")
+            console.error("Error detallado en handleMultipleQuestion:", error)
+            throw new Error("Error al generar la respuesta para muchas preguntas")
         }
     }
 }
