3b1f7d2642265c1a205f221912175701bd4c71c8
EleanQuintero <elean.quintero.madrid@gmail.com>
2025-10-24 12:54:13 +0200

    changed db config to env variables

--- src/infrastructure/db/mysql.ts
+++ src/infrastructure/db/mysql.ts
@@ -1,11 +1,15 @@
 import mysql from 'mysql2/promise'
-import { DB_DATA } from '../../consts/const'
+import { env } from '../../config/env'
+
+if (!env.DB_DATA.host || !env.DB_DATA.user || !env.DB_DATA.password || !env.DB_DATA.name || !env.DB_DATA.waitForConnections || !env.DB_DATA.connectionLimit) {
+    throw new Error('Missing required database configuration in environment variables')
+}
 
 export const pool = mysql.createPool({
-    host: DB_DATA.host,
-    user: DB_DATA.user,
-    password: DB_DATA.password,
-    database: DB_DATA.database,
-    waitForConnections: DB_DATA.waitForConnections, 
-    connectionLimit: DB_DATA.connectionLimit,
+    host: env.DB_DATA.host,
+    user: env.DB_DATA.user,
+    password: env.DB_DATA.password,
+    database: env.DB_DATA.name,
+    waitForConnections: env.DB_DATA.waitForConnections,
+    connectionLimit: env.DB_DATA.connectionLimit,
 })
